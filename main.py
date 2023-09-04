7413d1292a2e067d73add2a0cf20a5c13e3b7736
rupesh ahirrao <rupeshrao1397@gmail.com>
2023-09-04 03:10:51 -0700

    changes

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ with open('contact_data.csv', 'r') as file:
 import csv
 
 with open('contact_data.csv', mode='a') as file:
-    employee_writer = csv.writer(file)
+    writer = csv.writer(file)
 
-    employee_writer.writerow(['7','John Smith','jhonsmith@nsw.gov.au','male','07-09-1997','1 Nsk','7364829837']),
-    employee_writer.writerow(['8','Tony Smith','tonysmith@nsw.gov.au','male','15-07-1999','1 Nsk','7348298380']),
-    employee_writer.writerow(['9','Rogan Smith','rogansth@nsw.gov.au','male','18-08-1993','1 Nsk','7648298479']),
-    employee_writer.writerow(['10','Leo Smith','leosmith@nsw.gov.au','male','17-05-1987','1 Nsk','73648292379'])
+    writer.writerow(['7','John','jhonsmith@nsw.gov.au','male','07-09-1997','1 Nsk','7364829837']),
+    writer.writerow(['8','Tony','tonysmith@nsw.gov.au','male','15-07-1999','1 Nsk','7348298380']),
+    writer.writerow(['9','Rogan','rogansth@nsw.gov.au','male','18-08-1993','1 Nsk','7648298479']),
+    writer.writerow(['10','Leo','leosmith@nsw.gov.au','male','17-05-1987','1 Nsk','73648292379'])
 
     file.close()
 
